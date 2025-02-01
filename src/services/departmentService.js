d876540c1847e56e4f153fbdf732deec492fe809
Lucas <lucaslimacruz1@gmail.com>
2025-02-01 16:36:04 -0300

    remove junk files

--- src/services/departmentService.js
+++
@@ -1,9 +0,0 @@
-import BaseService from './baseService';
-class DepartmentService extends BaseService {
-    async getAllDepartments() {
-        const response = await this.apiClient.get('/departments');
-        return response.data?.result;
-    }
-}
-export default new DepartmentService();
-//# sourceMappingURL=departmentService.js.map
